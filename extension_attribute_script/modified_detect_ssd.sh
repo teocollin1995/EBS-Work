57ae8903a56629f7f3ffec334af724aeb28cc870
Teodoro Fields Collin <teocollin1995@gmail.com>
2015-09-21 15:59:07 -0400

    detect ssd

---
+++ extension_attribute_script/modified_detect_ssd.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#find the disk info
+disk0=`diskutil info disk0 | grep "Solid State" | awk '{ print $3 }'`
+disk1=`diskutil info disk1 | grep "Solid State" | awk '{ print $3 }'`
+#test results
+if [[ $disk0 == "Yes" ]]; then
+
+   echo "<result>disk0 is SSD</result>"
+   exit 0
+
+elif [[ $disk1 == "Yes" ]]; then
+
+   echo "<result>disk1 is SSD</result>"
+   exit 0
+
+fi
+
+echo "<result>No SSD Drives</result>"
+
+exit 0
